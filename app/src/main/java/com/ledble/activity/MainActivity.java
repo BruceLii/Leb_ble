e546a8fe4a88e7a251abb1f4b2a42ab86679160e
liyonlgin <liyonglin0704@163.com>
2017-10-11 23:11:03 +0800

    common

--- app/src/main/java/com/ledble/activity/MainActivity.java
+++ app/src/main/java/com/ledble/activity/MainActivity.java
@@ -189,7 +189,6 @@ public class MainActivity extends LedBleActivity implements NetExceptionInterfac
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 		setContentView(R.layout.activity_main);
 
 		mActivity = this;
